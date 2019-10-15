a660a2e1e923237a5b19bfe907d2b1efa1ee8110
Unknown <rrb@cin.ufpe.br>
2019-10-15 20:29:40 -0300

    Usar argparse e refatorar repetições em util.py

--- utils.py
+++ utils.py
@@ -1,39 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import argparse
 
-
-def main():
+def main(entrada, saida):
     # Trata o arquivo palavras.txt para remover os verbos (palavras terminadas em ar,er,ir)
-    with open('palavras.txt') as arquivo:
-        palavra = arquivo.readline()
-        palavra = palavra.lower()
-        palavra = palavra.rstrip()
+    start = time.time()
 
-        while palavra:
-            if palavra.endswith('ar'):
-                palavra = arquivo.readline()
-                palavra = palavra.lower()
-                palavra = palavra.rstrip()
-                continue
-
-            if palavra.endswith('er'):
-                palavra = arquivo.readline()
-                palavra = palavra.lower()
-                palavra = palavra.rstrip()
-                continue
-
-            if palavra.endswith('ir'):
-                palavra = arquivo.readline()
-                palavra = palavra.lower()
-                palavra = palavra.rstrip()
-                continue
-
-            print(palavra)
-
-            palavra = arquivo.readline()
-            palavra = palavra.lower()
-            palavra = palavra.rstrip()
-
+    # Ler todas as palavras do arquivo de entrada
+    with open(entrada, 'r') as arquivo:
+        palavras = [palavra.lower().rstrip() for palavra in arquivo.read().splitlines()]
+    
+    # Eliminar todas as palavras que são verbos
+    palavras_sem_verbos = [palavra for palavra in palavras if not palavra.endswith(('ar', 'er', 'ir'))]
+    
+    # Escrever todas as palavras que não são verbos no arquivo de saída
+    with open(saida, 'w') as f:
+        for palavra in palavras_sem_verbos:
+            f.write("%s\n" % palavra)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--entrada', type=str, required=True, default='palavras.txt', help='Arquivo de entrada contendo as palavras')
+    parser.add_argument('--saida', type=str, required=True, default='palavras_sem_verbos.txt', help='Arquivo de saída contendo as palavras sem verbos')
+
+    args = parser.parse_args()
+    entrada = args.entrada
+    saida = args.saida
+
+    main(entrada, saida)
